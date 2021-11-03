dfdfc464e51a9d92bfa11ffc65090682d5c0d8ce
janetgrr <90476485+janetgrr@users.noreply.github.com>
2021-11-03 09:09:08 -0600

    2.4 changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-console.log("Hi, Welcome to My Portfolio Site!");
-Code Integration <script
-			  src="https://code.jquery.com/jquery-3.6.0.min.js"
-			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-			  crossorigin="anonymous"></script>
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
